0c0e24b7be06b70ffc9f41ffe59991026f1fff45
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-09-21 08:45:29 +0300

    Added Axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,33 @@
-import './App.css';
-import Login from './AuthPages/Login';
-import Register from './AuthPages/Register';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import "./App.css";
+import Login from "./AuthPages/Login";
+import Register from "./AuthPages/Register";
+import axios from "axios";
+import { BrowserRouter } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Home from "./Home";
+
+//this line enables to use connect to server with axios
+axios.defaults.withCredentials = true;
 
 function App() {
+  const [isAuth, setisAuth] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8100/register/isLoggedIn")
+      .then(result => {
+        setisAuth(result.data);
+        console.log("Result : " + result.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/Register" element={<Register />}/>
-            <Route path="/Login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className="App">{isAuth ? <Home /> : <Register />}</div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>Welcome to home page user!!!</div>
+  )
+}
+
+export default Home
